8aeeb22098c10b2117e65a2edcc623f55cba8a99
Melis <meliskesen@gmail.com>
2024-04-15 21:20:33 +0300

    ✨

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "primereact/resources/primereact.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+import { Library } from './Library';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <Library></Library>
+    
     </div>
   );
 }

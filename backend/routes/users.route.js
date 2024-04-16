06bcdb33da6d55ee83ec3e3e90b3f80f08910e96
Melis Keşen <66209186+melis-kesen@users.noreply.github.com>
2024-04-16 12:25:56 +0300

    Update users.route.js

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -9,3 +9,42 @@ router.post('/users/:userId/borrow/:bookId', UserController.borrowBook);
 router.post('/users/:userId/return/:score', UserController.returnBook);
 
 module.exports = router;
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers", 
+      "Access-Control-Allow-Origin",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  /**
+   * Get all users 
+   */
+    app.get('/users/:userId',  UserController.getUsers);
+  
+  /**
+   * Get the user with given id
+   * @params id: id of the user
+   */
+  app.post('/users',  UserController.createUser);  
+  
+  /**
+   * Add new user
+   */
+  app.post('/users/:userId/borrow/:bookId',UserController.borrowBook);
+  
+  /** 
+ * Update the user with given id
+ * @params id: id of the user
+ */
+  app.post('/users/:userId/return/:score', UserController.returnBook);
+  
+  /**
+ * Delete the user with given id
+ * @params id: id of the user
+ */
+  app.delete("/api/users/:id", [authJwt.verifyToken, authJwt.isAdmin], controller.deleteUser);
+};

b60aca38bd0a2351a6d7de2fd4b28fd7990fa990
Melis Keşen <66209186+melis-kesen@users.noreply.github.com>
2024-04-17 10:07:17 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const app = express();
-const userRoutes = require('./routes/users.route');
-const bookRoutes = require('./routes/books.route');
-//const sequelize = require('./config/database'); 
+const express = require("express");
+const userRoutes = require("./routes/users.route");
+const bookRoutes = require("./routes/books.route");
+//const sequelize = require('./config/database');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-const dotenv = require('dotenv');
- 
- 
+const dotenv = require("dotenv");
+
 // Set environmental variables
 dotenv.config({
-  path: path.resolve(__dirname, `.env`)
+  path: path.resolve(__dirname, `.env`),
 });
 
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || "localhost";
 
+const app = express();
 // For forward client IP
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 
 global.__basedir = __dirname;
 // Veritabanı bağlantısını test etmek için basit bir sorgu yapın
@@ -32,9 +31,8 @@ global.__basedir = __dirname;
     console.error("Veritabanı bağlantı hatası:", err);
   });*/
 
-
-var corsOptions = { 
-  origin: [`http://${HOST}:${PORT}`, `http://${HOST}:3010`], // origin:"*",
+var corsOptions = {
+  origin: [`http://${HOST}:${PORT}`, `http://${HOST}:3000`],
   credentials: true,
 };
 
@@ -53,12 +51,12 @@ app.use(express.static(path.join(__dirname, "build")));
 // database
 //const db = require("./models");
 //veritabanı tabloları yoksa otomatik modellerden oluşturulur. bu sadece guncellenmesini sağlıyor.
-//db.sequelize.sync({ alter: true, schema: 'SL'}); 
+//db.sequelize.sync({ alter: false, schema: 'SL'});
 
 // routes
 // Kullanıcı rotalarını kullanmak için
-app.use("/users", userRoutes);
-app.use("/books", bookRoutes);
+require("./routes/users.route")(app);
+require("./routes/books.route")(app);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on ${HOST}:${PORT}.`);

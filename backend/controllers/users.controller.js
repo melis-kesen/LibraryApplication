b8a608e64c0bd2d56a05d284330f897c33d1f158
Melis Keşen <66209186+melis-kesen@users.noreply.github.com>
2024-04-17 10:06:57 +0300

    Update users.controller.js

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,25 +1,24 @@
-const db = require("../models"); 
+const db = require("../models");
 const User = db.user;
-const validateUser = require('../validators/users.validators');
-
+const validateUser = require("../validators/users.validators");
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.findAll();
-    res.json(users);
+    //const users = await User.findAll();
+    res.status(200).send("users");
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
+/*
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json(user);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 exports.borrowBook = async (req, res) => {
@@ -28,7 +27,7 @@ exports.borrowBook = async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 exports.returnBook = async (req, res) => {
@@ -37,7 +36,6 @@ exports.returnBook = async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
-};
-
+};*/
